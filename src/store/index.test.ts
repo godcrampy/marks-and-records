62c82b2f76220898ba2946ef45200a0dbc6efc23
Sahil <sahilbondre@gmail.com>
2020-04-16 11:24:47 +0530

    run lint

--- src/store/index.test.ts
+++ src/store/index.test.ts
@@ -3,33 +3,33 @@ import { createStore } from "redux";
 import { UserState } from "./user/types";
 import { setUser, removeUser } from "./user/actions";
 
-test('sets initial state values', () => {
+test("sets initial state values", () => {
   const store = createStore(rootReducer);
   const initialUserState: UserState = {
-    auth: false
-  }
+    auth: false,
+  };
   // * Test initial state
-  expect(store.getState().user).toEqual(initialUserState)
+  expect(store.getState().user).toEqual(initialUserState);
 });
 
-test('exports user reducers', () => {
+test("exports user reducers", () => {
   const store = createStore(rootReducer);
 
   // * Test adding user
   const user: User = {
     name: "John Doe",
     email: "john@doe.com",
-    id: "john"
-  }
+    id: "john",
+  };
   let expectedUserState: UserState = {
     auth: true,
-    user
-  }
+    user,
+  };
   store.dispatch(setUser(user));
   expect(store.getState().user).toEqual(expectedUserState);
 
   // * Test removing user
-  expectedUserState = { auth: false }
+  expectedUserState = { auth: false };
   store.dispatch(removeUser());
   expect(store.getState().user).toEqual(expectedUserState);
 });

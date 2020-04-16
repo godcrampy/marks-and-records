b6e4b8f0b4edb9540fb60e359f3b00eb7a520c4d
Sahil <sahilbondre@gmail.com>
2020-04-16 11:24:15 +0530

    add additional tests and coverage ignore flags

---
+++ src/store/index.test.ts
@@ -0,0 +1,35 @@
+import { rootReducer } from ".";
+import { createStore } from "redux";
+import { UserState } from "./user/types";
+import { setUser, removeUser } from "./user/actions";
+
+test('sets initial state values', () => {
+  const store = createStore(rootReducer);
+  const initialUserState: UserState = {
+    auth: false
+  }
+  // * Test initial state
+  expect(store.getState().user).toEqual(initialUserState)
+});
+
+test('exports user reducers', () => {
+  const store = createStore(rootReducer);
+
+  // * Test adding user
+  const user: User = {
+    name: "John Doe",
+    email: "john@doe.com",
+    id: "john"
+  }
+  let expectedUserState: UserState = {
+    auth: true,
+    user
+  }
+  store.dispatch(setUser(user));
+  expect(store.getState().user).toEqual(expectedUserState);
+
+  // * Test removing user
+  expectedUserState = { auth: false }
+  store.dispatch(removeUser());
+  expect(store.getState().user).toEqual(expectedUserState);
+});
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+
 import { combineReducers } from "redux";
 import { userReducer } from "./user/reducer";
 

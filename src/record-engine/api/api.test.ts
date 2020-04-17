5bb57b514db0127279bbc7dd46f168404315945c
Sahil <sahilbondre@gmail.com>
2020-04-17 12:05:54 +0530

    fix setRecord and add fetch recordByRange

--- src/record-engine/api/api.test.ts
+++ src/record-engine/api/api.test.ts
@@ -1,48 +1,56 @@
 import "../../config.expose";
 import Api from "./api";
 
-test("can write and fetch records", async () => {
-  const user: User = {
-    name: "John Doe",
-    email: "john@doe.com",
-    id: "john",
-  };
-  const api = new Api(user);
-  const record: DayRecord = {
-    date: +new Date(),
-    owner: user.id,
-    score: {
-      mood: 4,
-      work: 4,
+const user: User = {
+  name: "John Doe",
+  email: "john@doe.com",
+  id: "john",
+};
+const api = new Api(user);
+
+const record1: DayRecord = {
+  date: +new Date(),
+  owner: user.id,
+  score: {
+    mood: 4,
+    work: 4,
+  },
+  marks: [
+    { message: "abc", owner: user.id, metric: { mood: 4, work: 4 }, tags: [], time: +new Date() },
+  ],
+};
+
+const record2: DayRecord = {
+  date: +new Date() + 10, // The record of above test is still up
+  owner: user.id,
+  score: {
+    mood: 3,
+    work: 3,
+  },
+  marks: [
+    {
+      message: "cde",
+      owner: user.id,
+      metric: { mood: 3, work: 3 },
+      tags: [],
+      time: +new Date() + 10,
     },
-    marks: [
-      { message: "abc", owner: user.id, metric: { mood: 4, work: 4 }, tags: [], time: +new Date() },
-    ],
-  };
-  await api.setRecord(record);
+  ],
+};
+
+test("can write and fetch records", async () => {
+  await api.setRecord(record1);
   const records = await api.fetchRecords();
-  expect(records[0]).toEqual(record);
+  expect(records[0]).toEqual(record1);
 });
 
 test("fetches record chronologically ", async () => {
-  const user: User = {
-    name: "John Doe",
-    email: "john@doe.com",
-    id: "john",
-  };
-  const api = new Api(user);
-  const record: DayRecord = {
-    date: +new Date() + 10, // The record of above test is still up
-    owner: user.id,
-    score: {
-      mood: 3,
-      work: 3,
-    },
-    marks: [
-      { message: "cde", owner: user.id, metric: { mood: 3, work: 3 }, tags: [], time: +new Date() },
-    ],
-  };
-  await api.setRecord(record);
+  await api.setRecord(record2);
   const records = await api.fetchRecords();
-  expect(records[0]).toEqual(record);
+  expect(records[0]).toEqual(record2);
+});
+
+test("fetch records by range", async () => {
+  const records = await api.fetchRecordsByLimit(record1, 1);
+  expect(records[0]).toEqual(record2);
 });
--- src/record-engine/api/api.ts
+++ src/record-engine/api/api.ts
@@ -9,7 +9,10 @@ class Api {
   }
 
   async setRecord(record: DayRecord) {
-    await this.db.collection("records").doc(record.owner).set(record, { merge: true });
+    await this.db
+      .collection("records")
+      .doc(this.user.id + record.date.toString())
+      .set(record, { merge: true });
   }
 
   async fetchRecords(): Promise<DayRecord[]> {
@@ -17,6 +20,23 @@ class Api {
     const query = recordsRef.where("owner", "==", this.user.id);
 
     const doc = await query.orderBy("date", "desc").get();
+    return doc.docs.map((doc) => {
+      let data = doc.data();
+      const date = data.date;
+      const owner = data.owner;
+      const marks: Mark[] = data.marks;
+      const score: Score = data.score;
+      let record: DayRecord = { date, owner, marks, score };
+      return record;
+    });
+  }
+
+  async fetchRecordsByLimit(startAfter: DayRecord, limit: number): Promise<DayRecord[]> {
+    // Get limit records from start inclusive
+    const recordsRef = this.db.collection("records");
+    const query = recordsRef.where("owner", "==", this.user.id);
+
+    const doc = await query.orderBy("date", "desc").startAt(startAfter).limit(limit).get();
     return doc.docs.map((doc) => {
       let data = doc.data();
       const date = data.date;

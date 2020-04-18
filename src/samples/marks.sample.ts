fb32ef6b313147b3f6a6cd826b56f7ad92cd60a1
Sahil <sahilbondre@gmail.com>
2020-04-18 14:59:48 +0530

    add realistic sample

--- src/samples/marks.sample.ts
+++ src/samples/marks.sample.ts
@@ -4,17 +4,17 @@ const day1 = 1587131081609; // April 17 2020 19:14:41
 const day2 = 1587222195000; // April 18 2020 20:33:15
 
 const mark1: Mark = {
-  message: "abc",
+  message: "Had bath. Watermelon break. Gonna start #work on wiring redux to auth",
   owner: user.id,
-  metric: { mood: 4, work: 4 },
+  metric: { mood: 4, work: 0 },
   tags: [],
   time: day1,
 };
 
 const mark2: Mark = {
-  message: "cde",
+  message: "Completed redux with test. Gonna take #break",
   owner: user.id,
-  metric: { mood: 3, work: 3 },
+  metric: { mood: 3, work: 2 },
   tags: [],
   time: day2,
 };

dfcc2b5c219876ebabe96042e7f891885a43096a
Sahil <sahilbondre@gmail.com>
2020-04-18 22:36:44 +0530

    add addMark (without tests)

--- src/record-engine/record-engine.test.ts
+++ src/record-engine/record-engine.test.ts
@@ -7,7 +7,7 @@ import "../config.expose";
 test("dispatches records after fetching", async () => {
   const store = createStore(rootReducer);
   const engine = new RecordEngine(user, store);
-  await engine.initialize();
+  await engine.refresh();
 
   expect(store.getState().records.records.length).toBeGreaterThan(0);
 });
--- src/record-engine/record-engine.ts
+++ src/record-engine/record-engine.ts
@@ -1,24 +1,35 @@
 import Api from "./api/api";
-import { addRecords } from "../store/records/actions";
+import { addRecords, addMark } from "../store/records/actions";
 import { Store } from "redux";
+import { UserState } from "../store/user/types";
+import { RecordsState } from "../store/records/types";
 
 class RecordEngine {
   public user: User;
   public api: Api;
-  public store: Store;
+  public store: Store<{ user: UserState; records: RecordsState }>;
   constructor(user: User, store: Store) {
     this.user = user;
     this.store = store;
     this.api = new Api(user);
   }
 
-  async initialize() {
+  async refresh() {
     const fetchedRecords = await this.getRecords();
     this.store.dispatch(addRecords(fetchedRecords));
   }
 
   async getRecords(): Promise<DayRecord[]> {
     return this.api.fetchRecordsByLimit(7);
+  }
+
+  async addMark(mark: Mark) {
+    const date = new Date(mark.time).toDateString();
+    this.store.dispatch(addMark(mark));
+    const changedRecord: DayRecord = this.store.getState().records.records.find((r: DayRecord) => {
+      return r.date === date;
+    })!;
+    await this.api.setRecord(changedRecord);
   }
 }
 
